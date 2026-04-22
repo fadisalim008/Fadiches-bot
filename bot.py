0c883be03b21772fef595ffae302469d92f9020a
fadisalim008 <swedish.song.of.the.day@gmail.com>
2026-04-22 16:14:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8782424758:AAHrJzl-VGFjVLbBqGmzfdU10sCO-HahYtk"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("البوت اشتغل بنجاح ✅")
+    await update.message.reply_text("اشتغل البوت بنجاح ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
